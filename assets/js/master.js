054ebb71d96221e3fc99bc4d82da1fe4f24b8eeb
AWITI0301 <johnoketchawiti@gmail.com>
2022-05-15 01:38:08 +0300

    modified assets

--- assets/js/master.js
+++ assets/js/master.js
@@ -1,6 +1,15 @@
 function check() {
     var c=0;
     var q1=document.quiz.question1.value;
+    var q2=document.quiz.question1.value;
+    var q3=document.quiz.question1.value;
+    var q4=document.quiz.question1.value;
+    var q5=document.quiz.question1.value;
+    var q6=document.quiz.question1.value;
+    var q7=document.quiz.question1.value;
+    var q8=document.quiz.question1.value;
+    var q9=document.quiz.question1.value;
+    var q10=document.quiz.question1.value;
     if (q1=="Web") (c++)
         document.write(c)
 }
