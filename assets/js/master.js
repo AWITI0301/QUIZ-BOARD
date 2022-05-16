d617ff558c2ee2c63149f332778eb607741eea2e
AWITI0301 <johnoketchawiti@gmail.com>
2022-05-17 00:11:42 +0300

    reduce the number of questions

--- assets/js/master.js
+++ assets/js/master.js
@@ -1,56 +1,48 @@
+function yourScore(){
+    // alert("Good work")
 
-function check() {
+    var quiz1=document.myForm.question1.value;
+    var quiz2=document.myForm.question2.value;
+    var quiz3=document.myForm.question3.value;
+    var quiz4=document.myForm.question4.value;
+    var quiz5=document.myForm.question5.value;
+    var result= 0;
 
-    var c=0;
-    var q1=document.quiz.question1.value;
-    var q2=document.quiz.question2.value;
-    var q3=document.quiz.question3.value;
-    var q4=document.quiz.question4.value;
-    var q5=document.quiz.question5.value;
-    var q6=document.quiz.question6.value;
-    var q7=document.quiz.question7.value;
-    var q8=document.quiz.question8.value;
-    var q9=document.quiz.question9.value;
-    var q10=document.quiz.question10.value;
-    var number_correct=document.getElementById("number_correct");
-    var quiz=document.getElementById(quiz);
-     
-    if (q1 =="Web") (c++)
-    if (q2=="Object-based") (c++)
-    if (q3=="It is a scripting language used to make the website interactive") (c++)
-    if (q4=="JavaScript file and in HTML document directly") (c++)
-    if (q5=="Semicolon (;)") (c++)
-    if (q6=="All of the above") (c++)
-    if (q7=="getElementById(id)") (c++)
-    if (q8=="innerHTML") (c++)
-    if (q9=="document.write()") (c++)
-    if (q10=="console.log()") (c++)
-    
-    document.write(c)
-
-    
-    
-
-    var messages=["Good perfomance", "Average perfomance, there is room for improvement","poor perfomance, you really need to do more on your class work"];
-
-    var range;
-         if(correct<4) {
-             range=2;
-         }
-         if(correct>4 && correct<=7) {
-             range=1;
-         }
-         if (correct>7) {
-             range=0;
-         }
+  var displayForm=document.getElementById("displayForm")
+  var display=document.getElementById("display")
+  display.style.display="visible"
 
 
+    if(quiz1=="C"){ result++ }
+    if(quiz2=="B"){ result++ }
+    if(quiz3=="A"){ result++ }
+    if(quiz4=="C"){ result++ }
+    if(quiz5=="D"){ result++ }
 
-   
-    document.getElementById("message").innerHTML=messages(range);
-   
-    quiz.style.display="none"
 
-    document.getElementById("after_submit").style.visibility="visible";
-    
+    if(quiz1=="" || quiz2=="" || quiz3=="" || quiz4=="" || quiz5=="" ){
+        alert("Please Answer All the questions")
+    }
+    var finalScore= result*20;
+    if( finalScore>=80){
+        displayForm.innerHTML= "Your Score is " + finalScore + "%" + "   "  + "Excellent"
+
+    }
+    else if( finalScore>=60 && finalScore<80){
+        displayForm.innerHTML= "Your Score is " + finalScore + "%" + "  "  + "Fairly Done"
+
+    }
+    else if ( finalScore>=40 && finalScore<60){
+        displayForm.innerHTML="Your Score is " + finalScore + "%"  + "  " + "Not Bad"
+
+    }
+    else if ( finalScore>=0 && finalScore<40){
+        displayForm.innerHTML="Your Score is " + finalScore + "%"   + "  " + "Try Again!"
+
+    }
+  
+
 }
+function newComments(){
+    var displayComments=document.getElementById("viewComments")
+    displayComments.innerHTML="Thank you for taking your time to attempt this questions"}
